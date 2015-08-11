2e82827b842a110d648694f7f8544f66eb99cbca
Kalman Kepes <kepeskn@hspc04.informatik.uni-stuttgart.de>
2015-08-11 16:01:02 +0200

    changes way of fetching own remote address

--- src/main/java/iaas/uni/stuttgart/de/srs/config/Configuration.java
+++ src/main/java/iaas/uni/stuttgart/de/srs/config/Configuration.java
@@ -1,6 +1,11 @@
 package iaas.uni.stuttgart.de.srs.config;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.UnknownHostException;
 
 import javax.ws.rs.core.Context;
@@ -17,8 +22,19 @@ public class Configuration {
 	public String getSitOPTSRSServiceAddress(){
 		// TODO pretty weak check for the address of this service
 		try {
-			return InetAddress.getLocalHost().getHostAddress() + ":8080/srsTestService";
+			
+			// taken from http://stackoverflow.com/a/2939223
+			URL whatismyip = new URL("http://checkip.amazonaws.com");
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+			                whatismyip.openStream()));
+
+			String ip = in.readLine();
+			return "http://" + ip + ":8080/srsTestService";
 		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return null;
